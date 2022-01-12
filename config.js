3232b83518872ac0acf0a6f1d0bf395d1c2b5feb
Nguyễn Ngọc Thủy <62063325+seiten0601@users.noreply.github.com>
2022-01-12 20:58:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    titleWeb: "Zui lên ik nèeee",
+    introTitle: 'Nguyễn Ngọc Uyên Thao',
+    introDesc: `Cho dù hoàn cảnh hiện tại của em có tồi tệ thế nào đi nữa,
+    thì em vẫn lun có tui ở đây để làm em vui lên nèee.`
+    btnIntro: 'Chơi thử nào',
+    title: 'Mong người đọc được dòng này sẽ diệt trừ được những cảm xúc tiêu cực của bản thân nè 🥰',
+    desc: 'Cố lên em nhó , Iu em nhìu ',
+    btnYes: 'Sẽ cố <33',
+    btnNo: 'Không nha (;¬_¬)',
+    question: 'Anh biết em sẽ muổn bản thân tự vượt qua những lúc như thế này, nhưng hãy để anh giúp em vượt qua nó nhé <3',
+    btnReply: 'Nói cho anh đi nà <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Mặc dù trang web này cũng không có gì nhiều.',
+    messDesc: 'Mong em sẽ vượt qua được và tiếp tục cười với anh nhiều hơn nhéeee',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100025696363821' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
